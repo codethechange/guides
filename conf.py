3b480d2761afefad40f32ed76fbbb80a997bc62c
Drew Gregory <djgregny@gmail.com>
2021-05-16 15:34:34 -0700

    warning fixes, updates, very beginnings of js workshop

--- conf.py
+++ conf.py
@@ -41,6 +41,7 @@ release = ''
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
